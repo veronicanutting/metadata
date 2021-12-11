aa9f00e70f8ef1d741cda4ab80aaf8606141e9d2
Veronica Nutting <veronicanutting@Veronicas-New-MacBook-Air.local>
2021-12-11 14:17:07 -0500

    making changes to structure

--- meta_parser.py
+++ meta_parser.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 directory_path = os.getcwd()
 folder = directory_path+"/test_files"
+#folder = "/Users/veronicanutting/Desktop/general"
 
 # Get all file paths within given directory
 def getFilePaths(folder):
@@ -46,30 +47,65 @@ def preProcess(stdout_data):
 
 # Aggregate counts for metadata tags
 def getStats(file_paths,folder):
-    p_counts = {}
-    p_vals = {}
+    t_counts = {}
+    t_vals = {}
+    file_tags = {}
     for path in file_paths:
+        file_tags[path] = [0,[]]
         stdout_data = readMetadata(folder+"/"+path)
         meta_dict = preProcess(stdout_data)
         for k,v in meta_dict.items():
-            if k in p_counts.keys():
-                p_counts[k]+=1
-                p_vals[k].append(v)
+            if k in t_counts.keys():
+                t_counts[k]+=1
+                t_vals[k].append(v)
             else:
-                p_counts[k]=1
-                p_vals[k]=[v]
-    return p_counts,p_vals
+                t_counts[k]=1
+                t_vals[k]=[v]
+        file_tags[path][0]=t_counts[k]
+        file_tags[path][1]=set(t_counts.keys())
+    return t_counts,t_vals,file_tags
+
+# From tag counts, output frequency graph
+def tagFrequencies(t_counts,file_paths,orderByBar=False):
+    if orderByBar:
+        names,values=[],[]
+        for k in sorted(t_counts, key=t_counts.get):
+            names.append(k),values.append(t_counts[k])
+    else:
+        names = sorted(list(t_counts.keys()))
+        values = [t_counts[k] for k in names]
+    plt.bar(range(len(t_counts)), values)#, tick_label=names)
+
+    if len(names)<=10:
+        plt.xticks(range(len(t_counts)), names, rotation=90)
+    plt.ylabel("Tag Counts")
+    plt.title(f"Metadata Tag Frequencies\n{len(names)} Tags from {len(file_paths)} Files")
+    plt.tight_layout()
+    plt.savefig('freqs.png')
+
+# Get other tag stats
+def findSharedTags(file_tags):
+    num_files = len(file_tags)
+
+    # Find tags shared among all files
+    all_tags = [file[1] for file in file_tags.values()]
+    shared_tags = set.intersection(*all_tags)
+    print(f"{num_files} files share {len(shared_tags)} tags.")
+    print(shared_tags)
+
+def generalStats(t_vals):
+    print(t_vals.keys())
+    print(t_vals['kMDItemKind'])
+    #print(f"{len(t_vals["kMDItemKind"])} types of files.")
+    #print(t_vals["kMDItemKind"])
+
+    #print(f"User id")
+    #print(unique(t_vals["kMDItemFSOwnerUserID"]))
 
 # Get stats for all test files in folder
 file_paths = getFilePaths(folder)
-p_counts,p_vals = getStats(file_paths,folder)
+t_counts,t_vals,file_tags = getStats(file_paths,folder)
 
-# Output as graph
-names = sorted(list(p_counts.keys()))
-values = [p_counts[k] for k in names]
-plt.bar(range(len(p_counts)), values)#, tick_label=names)
-plt.xticks(range(len(p_counts)), names, rotation=90)
-plt.ylabel("Frequencies")
-plt.title(f"Metadata Tag Frequencies\n{len(names)} Tags from {len(file_paths)} Files")
-plt.tight_layout()
-plt.savefig('stats.png')
+#tagFrequencies(t_counts,file_paths,orderByBar=True)
+findSharedTags(file_tags)
+#generalStats(t_vals)
